c313ec0c4e61f5d8ffe970d3f9c58e12a170f5ab
al394923 <al394923@uji.es>
2021-03-16 12:04:12 +0100

    documentacion

---
+++ src/main/java/Proyecto/Tareas/Resultado/Documentación.java
@@ -0,0 +1,23 @@
+package Proyecto.Tareas.Resultado;
+
+public class Documentación {
+    private String campo;
+    private int numpaginas;
+    private int espaciodisc;
+
+    public Documentación(){}
+
+    public Documentación(String campo, int numpaginas, int espaciodisc) {
+        this.campo = campo;
+        this.numpaginas = numpaginas;
+        this.espaciodisc = espaciodisc;
+    }
+
+    public String getCampo(){ return campo; }
+    public int getNumpaginas(){ return numpaginas; }
+    public int getEspaciodisc(){ return espaciodisc; }
+
+    public String toString(){
+        return "El campo utilizado es: "+ campo +"\nEl número de págians es:"+numpaginas+"\nEl espacio en el disco es:"+espaciodisc;
+    }
+}
