3043ffb68282b5ef83e2fa8b445d3cf93139cc7a
DragoDeveloper <al394814@uji.es>
2021-03-16 12:02:18 +0100

    Añado la clase PáginaWeb

---
+++ src/main/java/Proyecto/Tareas/Resultado/PáginaWeb.java
@@ -0,0 +1,27 @@
+package Proyecto.Tareas.Resultado;
+
+public class PáginaWeb {
+
+    boolean tipo; //True --> estática || false--> dinámica
+    String lenguaje;
+    String tipoBackend;
+
+    public PáginaWeb(boolean tipo,String lenguaje,String tipoBackend) {
+        this.tipo = tipo;
+        this.lenguaje = lenguaje;
+        this.tipoBackend=tipoBackend;
+    }
+
+    public boolean isTipo() {
+        return tipo;
+    }
+
+    public String getLenguaje() {
+        return lenguaje;
+    }
+
+    public String getTipoBackend() {
+        return tipoBackend;
+    }
+
+}
