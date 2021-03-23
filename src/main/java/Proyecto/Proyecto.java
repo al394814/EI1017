c6200d12040e2d7756bb84c6f0918d859eda0158
tonis <al394923@uji.es>
2021-03-23 11:58:41 +0100

    config1

--- src/main/java/Proyecto/Proyecto.java
+++ src/main/java/Proyecto/Proyecto.java
@@ -3,26 +3,30 @@ package Proyecto;
 import Proyecto.Personas.Persona;
 import Proyecto.Tareas.Tareas;
 
+import java.util.List;
+
 public class Proyecto {
 
     private String nombre;
-    private Persona persona;
-    private Tareas tareas;
+    private List<Persona> persona;
+    private List<Tareas> tareas;
 
 
 
     public Proyecto(){
-
     }
 
-    public  Proyecto(String nombre,Persona persona,Tareas tareas){
+    public  Proyecto(String nombre,List<Persona> persona,List<Tareas> tareas){
         this.nombre=nombre;
         this.persona=persona;
         this.tareas=tareas;
     }
 
-    public Persona getPersona(){
+    public List<Persona> getPersona(){
         return persona;
-    }//devuelvo la lista pero si no es asi lo cambiare
+    }
+
+    public List<Tareas> getTareas() { return tareas; }
 }
+
 
