8c1b0024682356f2ff0b5612929936954e94e7c3
tonis <al394923@uji.es>
2021-03-29 20:55:44 +0200

    test2

--- src/test/java/Proyecto/ProyectoTest.java
+++ src/test/java/Proyecto/ProyectoTest.java
@@ -1,8 +1,10 @@
 package Proyecto;
 
+import Proyecto.Personas.Persona;
 import Proyecto.Tareas.Tarea;
 import org.junit.jupiter.api.Test;
 
+import java.net.HttpRetryException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,9 +21,19 @@ class ProyectoTest {
 
     @Test
     void getPersona() {
+        Proyecto proyecto = new Proyecto();
+        List<Persona> l = new ArrayList<>();
+        assertEquals(l, proyecto.getPersona());
+
     }
 
-    @Test
-    void añadirPersonaProyecto() {
+    void añadirPersonaProyecto(){
+        Proyecto proyecto = new Proyecto();
+        String dni= "tretre";
+        Persona p = new Persona("paco", dni, "fsef");
+        proyecto.añadirPersonaProyecto(p);
+        proyecto.EncontramosDniEnLasPersonasDelProyecto(dni);
+        assertEquals(proyecto.EncontramosDniEnLasPersonasDelProyecto(dni), p);
     }
+    // En el proyecto no se pueden dar de baja personas
 }
