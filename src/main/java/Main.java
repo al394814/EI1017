fcaabc03cf8bca096b681d65a6fbb54992f81728
DragoDeveloper <al394814@uji.es>
2021-04-19 23:45:02 +0200

    ESTE SE LO MANDO AL PROFE

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import EntradaSalida.IniciarNuevoProyecto;
 
+import java.io.IOException;
+
 import static Interfaz.MenuPrograma.MenúPrograma;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         System.out.println("\nVAS A INICIAR UN PROYECTO\n");
         MenúPrograma(IniciarNuevoProyecto.iniciarNuevoProyecto());
     }
